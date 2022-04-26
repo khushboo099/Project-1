7a6fe23749849de92de0b0660ee3e19755eda48f
ZUBERKHAN034 <50729008+ZUBERKHAN034@users.noreply.github.com>
2022-04-26 19:30:25 +0530

    Update authorController.js
    UPDATE BASE FORMAT

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,17 +3,3 @@ const AuthorModel = require('../models/authorModel')
 
 ///////////////// [ ALL HANDLER LOGIC HERE ] /////////////////
 
-const authorSchema = new mongoose.Schema({
-
-
-
-
-
-
-
-
-
-
-
-},{timestamps:true})
-module.exports = mongoose.model("Author", authorSchema)
