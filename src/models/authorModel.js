f628edf85e09c47539752aaf586c6b6c1eb51342
Zuber Khan <Zuberkhan034@gmail.com>
2022-04-26 21:24:14 +0530

    Create authorModel & add some missing field in blogModel

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -3,20 +3,28 @@ const { default: isEmail } = require("validator/lib/isEmail")
 
 ///////////////// [ AUTHOR SCHEMA HERE ] /////////////////
 const authorSchema = new mongoose.Schema({
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        enum:["Mr","Mrs","Miss"]
+    },
+    email:{
+        type:String,
+        unique:true,
+        validate:[isEmail,'invalid email']
+        
+    },
+    password:{
+        type:String,
+        required:true
+    }
 },{timestamps:true})
+
 module.exports = mongoose.model("Author", authorSchema)
--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,31 +1,42 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const objectId = mongoose.Schema.Types.ObjectId;
+
 ///////////////// [ BLOG SCHEMA HERE ] /////////////////
 const blogSchema = new mongoose.Schema({
-    
-        title: {type:String,
-            required:true
-        },
-
-        body: {type:String,
-            required:true
-        },
-        authorId:{
-            type:ObjectId,
-            ref:Author
-        },
-        tags:[String],
-        category: {type:[String],
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-        subcategory:[String],
-        isPublished: {type:Boolean,
-        default:false
+    body: {
+        type: String,
+        required: true
     },
-        publishedAt:String, // if published is true publishedAt will have a date 2021-09-17T04:25:07.803Z
-        isDeleted: {type:Boolean,
-        default:false
+    authorId: {
+        type: objectId,
+        required: true,
+        ref: ('Author')
     },
-        deletedAt:String , // if deleted is true deletedAt will have a date 2021-09-17T04:25:07.803Z,
-},{timestamps:true})
+    tags: [String],
+    category: {
+        type: [String],
+        required: true
+    },
+    subcategory: [String],
+    deletedAt: {
+        type: Date
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    publishedAt: {
+        type: Date
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    }
+},
+    { timestamps: true }
+)
 module.exports = mongoose.model("Blog", blogSchema)
